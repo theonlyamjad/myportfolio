e1b0436991e223b4f20d77064c8ca3d2347651e4
theonlyamjad <amjadmessi2004@gmail.com>
2025-06-28 14:37:38 +0100

    add typescript

--- components/SkillIcon.tsx
+++ components/SkillIcon.tsx
@@ -10,12 +10,12 @@ export const SkillIcon = ({ name }: { name: string }) => {
     "HTML": <Icon icon="logos:html-5" className="w-6 h-6" />,
     "CSS": <Icon icon="logos:css-3" className="w-6 h-6" />,
     "JavaScript": <Icon icon="logos:javascript" className="w-6 h-6" />,
+    "TypeScript": <Icon icon="logos:typescript" className="w-6 h-6" />,
     "React.js": <Icon icon="logos:react" className="w-6 h-6" />,
     "Next.js": <Icon icon="logos:nextjs-icon" className="w-6 h-6" />,
     "Tailwind CSS": <Icon icon="logos:tailwindcss-icon" className="w-6 h-6" />,
     "Bootstrap": <Icon icon="logos:bootstrap" className="w-6 h-6" />,
     "Shadcn UI": <Icon icon="simple-icons:shadcnui" className="w-6 h-6" />,
-    "Magicui": <Icon icon="simple-icons:magicui" className="w-6 h-6"/>,
 
     // Backend
     "PHP": <Icon icon="logos:php" className="w-6 h-6" />,
