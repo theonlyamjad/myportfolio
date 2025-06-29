8a3ea51e77ef3e531457b25ec616909ebaa165d8
theonlyamjad <amjadmessi2004@gmail.com>
2025-06-29 13:07:23 +0100

    add vercel

--- components/SkillIcon.tsx
+++ components/SkillIcon.tsx
@@ -28,7 +28,7 @@ export const SkillIcon = ({ name }: { name: string }) => {
 
     // Tools
     "Git & GitHub": <Icon icon="logos:git-icon" className="w-6 h-6" />,
-    "Vercel": <Icon icon="ri:vercel-fill" className="w-6 h-6" />,
+    "Vercel": <Icon icon="logos:vercel" className="w-6 h-6" />,
     "PhpMyAdmin": <Icon icon="logos:mysql-icon" className="w-6 h-6" />,
     "cPanel": <Icon icon="logos:cpanel" className="w-6 h-6" />,
     "WordPress": <Icon icon="logos:wordpress-icon" className="w-6 h-6" />,
